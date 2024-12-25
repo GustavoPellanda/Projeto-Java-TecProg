b72215c7b173bd089196ad55f561554c174a2b53
GustavoPellanda <gusfipe@gmail.com>
2024-12-25 19:28:26 -0300

    Main refatorada.

---
+++ src/App.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+public class App {
+    private FacultyList facultyList;
+    private GUIManager guiManager;
+    private SaveManager saveManager;
+
+    public App() {
+        saveManager = new SaveManager();
+        facultyList = new FacultyList();
+        guiManager = new GUIManager(facultyList);
+    }
+
+    // Loads the saved faculty list into the facultyList
+    private void loadSavedFacultyList() {
+        List<String> savedList = saveManager.loadFacultyList();
+        for (String name : savedList) {
+            facultyList.addFaculty(name);
+        }
+    }
+
+    // Creates the GUI
+    private void createAndShowGUI() {
+        guiManager.createAndShowGUI();
+    }
+
+    // Saves the faculty list when the application is closed
+    private void setupShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            saveManager.saveFacultyList(facultyList.getFacultyList());
+        }));
+    }
+
+    public void start() {
+        loadSavedFacultyList();
+        createAndShowGUI();
+        setupShutdownHook();
+    }
+}
